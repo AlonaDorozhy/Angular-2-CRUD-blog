cf01be5aee95b09a76ec8aa8488b22edf3869e27
Alona <dorozhynskaad@gmail.com>
2019-03-31 19:52:00 +0300

    complete

--- src/app/posts/post-dashbord/post-dashbord.component.ts
+++ src/app/posts/post-dashbord/post-dashbord.component.ts
@@ -47,7 +47,6 @@ export class PostDashbordComponent implements OnInit {
     this.content = ''
     this.image = ''
 
-    this.saving = 'Post Created!'
     setTimeout(() => (this.saving = 'Create Post'), 3000)
 
     this.router.navigate(['/blog']);
@@ -80,18 +79,6 @@ export class PostDashbordComponent implements OnInit {
   }
 
 
-  // uploadImage(event) {
-  //   const file = event.target.files[0]
-  //   const path = `posts/${file.name}`
-  //   if (file.type.split('/')[0] !== 'image') {
-  //     return alert('only image files')
-  //   } else {
-  //     const task = this.storage.upload(path, file)
-  //     this.downloadURL = task.downloadURL()
-  //     this.uploadPercent = task.percentageChanges()
-  //     console.log('Image Uploaded!')
-  //     this.downloadURL.subscribe(url => (this.image = url))
-  //   }
-  // }
+
 
 }
--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { PostService } from '../post.service'
 import { Post } from '../post'
 import { AuthService } from '../../core/auth.service'
+import { Observable } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-detail',
@@ -14,12 +17,19 @@ export class PostDetailComponent implements OnInit {
   post: Post;
   editing: boolean = false;
  
+  content: string
+  image: string
+  title: string
+  uploadPercent: Observable<number>
+  downloadURL: Observable<string>
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private postService: PostService,
-    public auth: AuthService
+    private auth: AuthService,
+    private storage: AngularFireStorage,
+   
   ) { }
 
   ngOnInit() {
@@ -36,13 +46,37 @@ export class PostDetailComponent implements OnInit {
     const formData = {
       title: this.post.title,
       content: this.post.content,
-      image: this.post.image
+      image: this.image,
     }
 
-    
     const id = this.route.snapshot.paramMap.get('id');
     this.postService.update(id, formData);
     this.editing = false
+  }
+
+  uploadImage(event) {
+
+    let file = event.target.files[0];
+    let path = `posts/${file.name}`;
+    if (file.type.split('/')[0] !== 'image') {
+      return alert('Add image');
+    } else {
+
+      let ref = this.storage.ref(path);
+      let task = this.storage.upload(path, file);
+      this.uploadPercent = task.percentageChanges();
+      console.log('Success');
+      task.snapshotChanges().pipe(
+        finalize(() => {
+          this.downloadURL = ref.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            this.image = url
+
+          });
+        }
+        )
+      ).subscribe();
+    }
   }
 
   delete() {
