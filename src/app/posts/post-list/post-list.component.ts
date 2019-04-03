24ff9d074faa06e712c6d74310b9813ba093258f
Alona <dorozhynskaad@gmail.com>
2019-04-03 17:26:15 +0300

    I did it 1

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -5,19 +5,38 @@ import { Post } from '../post'
 import { AuthService } from '../../core/auth.service'
 import { from } from 'rxjs';
 import { Router } from '@angular/router';
+import * as _ from 'lodash'; 
+  import { Pipe, PipeTransform } from '@angular/core';
+import { PostsModule } from '../posts.module';
+import { post } from 'selenium-webdriver/http';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+
+  @Pipe({
+    name: 'unique',
+    pure: false
+  })
+
+export class PostListComponent implements OnInit  {
   posts: Observable<Post[]>
+ 
   constructor(private postService: PostService, public auth: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.posts = this.postService.getPosts()
+    
+  
   }
 
+  trackElement() {
+  
+    return 
+  }
+  
   delete(id: string) {
     this.postService.delete(id)
       .then(
@@ -30,5 +49,5 @@ export class PostListComponent implements OnInit {
       )
   }
 
-
+ 
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../core/auth.service'
+import { Post } from 'src/app/posts/post';
+import { Observable } from 'rxjs';
+import { PostService } from 'src/app/posts/post.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  logoImg: any = "../img/logo.png";
-  constructor(public auth: AuthService) { }
+  posts: Observable<Post[]>
+  constructor(private postService: PostService, public auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.posts = this.postService.getPosts()
+     
+    console.log(this.posts)
+  }
+
+  delete(id: string) {
+    this.postService.delete(id)
+      .then(
+        () => {
+          this.router.navigate(['/blog']);
+        },
+        err => {
+          console.log(err);
+        }
+      )
   }
 
 }
